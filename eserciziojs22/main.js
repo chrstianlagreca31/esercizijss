ec8f44655f9c18dfc788bf01788a806afdf93b7f
chrstianlagreca31 <chris06lagreca@gmail.com>
2025-02-24 17:34:04 +0100

    Add files via upload

---
+++ eserciziojs22/main.js
@@ -0,0 +1,18 @@
+
+let num1 = prompt("Inserisci il primo numero:");
+let num2 = prompt("Inserisci il secondo numero:");
+
+
+let somma = num1 + num2;
+let sottrazione = num1 - num2;
+let moltiplicazione = num1 * num2;
+let divisione =  num1 / num2 ;
+let potenza = Math.pow(num1, num2);
+
+
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono:`);
+console.log(`Somma: ${somma}`);
+console.log(`Sottrazione: ${sottrazione}`);
+console.log(`Moltiplicazione: ${moltiplicazione}`);
+console.log(`Divisione: ${divisione}`);
+console.log(`Potenza: ${potenza}`);
